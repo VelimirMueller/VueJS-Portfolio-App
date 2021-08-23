3c65ba978bc30446a9653905da96cc9ed4ba0956
unknown <velimir.mueller@googlemail.com>
2021-08-23 21:59:22 +0200

    fixed some typos and changed some text

--- src/component_data/textdata.js
+++ src/component_data/textdata.js
@@ -1,32 +1,32 @@
 export const TextData = {
   block_1: [
     'Hello dear visitor, good to have you here!',
-    'My name is Velimir Müller, I am from Berlin and a german native speaker. Since July 2021 I am an graduated IT specialist for application development.',
-    'Now I am looking for a Junior Web Developer position and maybe you are my future employer so I am happy you are visiting this page.',
+    "My name is Velimir Müller and I'm from Berlin and a German native speaker. Since July 2021 I'm a graduated IT specialist for application development.",
+    'Now I am looking for a Junior Web Developer position, and maybe you are my future employer, so I am happy you are visiting this page.',
     '',
     'READ MORE'],
 
   block_2: [
     'Why should you hire me?',
-    'Because I love what I do and thats why I am constantly learning and improving myself in the web development field.',
+    'Because I love what I do and that is why I am constantly learning and improving myself in the web development field.',
     'I am not afraid to dive into new technologies because developing software is not just my profession of choice but also my hobby.',
-    'Besides that I have strong social skills and think of myself as an absolute teamplayer.',
+    'Besides that, I have strong social skills and think of myself as an absolute team player.',
     'READ MORE'
   ],
 
   block_3: [
     'What are my technical skills?',
-    'For most projects I have used the laravel framework for my backend but recently I switched to Symfony because I just like it more and I feel more comfortable with it.I also really enjoyed to work with templating engines like Blade or Twig.',
-    'I was able to get some hands-on experience with bootstrap, tailwindcss and SCSS too.',
+    'For most projects, I have used the laravel framework for my backend but recently I switched to Symfony because I just like it more and I feel more comfortable with it. I also really enjoyed working with templating engines like Blade or Twig.',
+    'I was able to get some hands-on experience with bootstrap, tailwindcss, and SCSS, too.',
     'When it comes to JavaScript I am not afraid to try new libraries or frameworks like Vue.js or Chart.js',
     'READ MORE'
   ],
 
   block_4: [
     'What else?',
-    'I like working with CLIs and I had the opporunity to work with Linux server systems during my intern.',
-    'I am also a huge fan of the Raspberry Pie and have set up alot of servers on it already.',
-    'Also I have some knowledge about SQL and I was using Figma for some pictures of this site.',
+    'Working with CLIs feels natural to me and I had the opporunity to work with Linux server systems during my intern.',
+    'I am also a big fan of the Raspberry Pie and have set up several of servers on it already.',
+    'Also, I have some knowledge about SQL and I was using Figma for some pictures of this site.',
     'TECHNOLOGIES USED',
   ],
 };
